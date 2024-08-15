39d3d57d94f2bf39c70dd2d2026d100128ce3053
Коровяковский Никита Дмитриевич <korovyakovskijnd@cifra-k.ru>
2024-08-15 19:27:17 +0700

    fix: Учтены пожелания

--- main.py
+++ main.py
@@ -1,48 +1,60 @@
-from fastapi import FastAPI, HTTPException
-import redis
-import logging
+from fastapi import FastAPI, HTTPException, Depends
+import redis.asyncio as redis
+from models import Address, MessageResponse, Settings, WriteAddress
+from typing import AsyncGenerator
 
-from models import Address, WriteAddress
-from settings import REDIS_DB, REDIS_HOST, REDIS_PORT
 
-logger = logging.getLogger("redis")
+settings = Settings()
 
-try:
-    redis_connection = redis.StrictRedis(
-        host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True
+
+async def get_redis_connection() -> AsyncGenerator[redis.Redis, None]:
+    redis_connection = redis.Redis(
+        host=settings.redis_host,
+        port=settings.redis_port,
+        db=settings.redis_db,
+        decode_responses=True
     )
-except redis.ConnectionError as e:
-    logger.error(f"Ошибка подключения к Redis: {e}")
+    try:
+        yield redis_connection
+    finally:
+        await redis_connection.close()
 
 
 app = FastAPI()
 
 
-@app.post("/write_data/", response_model=dict, summary="Добавить новый адрес")
-def write_address(data: WriteAddress) -> dict:
+@app.post(
+    "/write_data/",
+    response_model=MessageResponse,
+    status_code=201,
+    summary="Добавить или обновить адрес",
+)
+async def write_or_update_address(
+    data: WriteAddress,
+    redis_conn: redis.Redis = Depends(get_redis_connection),
+) -> MessageResponse:
     try:
-        redis_connection.set(data.phone, data.address)
-        return {"message": "Адрес успешно добавлен"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Oшибка: " + str(e))
+        existing_address = await redis_conn.get(data.phone)
+
+        if existing_address:
+            await redis_conn.set(data.phone, data.address)
+            return MessageResponse(message="Адрес успешно обновлен")
+        else:
+            await redis_conn.set(data.phone, data.address)
+            return MessageResponse(message="Адрес успешно добавлен")
+    except redis.ConnectionError:
+        raise HTTPException(status_code=500, detail="Ошибка подключения к Redis")
 
 
-# Хоть редис и автоматически обновляет значение по ключу, но разделил для использования put запроса
-@app.put("/write_data/", response_model=dict, summary="Обновить адрес")
-def update_address(data: WriteAddress) -> dict[str]:
+@app.get("/check_data/", status_code=200, summary="Проверить адрес")
+async def get_address(
+    phone: str,
+    redis_conn: redis.Redis = Depends(get_redis_connection),
+) -> Address | dict:
     try:
-        redis_connection.set(data.phone, data.address)
-        return {"message": "Адрес успешно обновлен"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Oшибка: " + str(e))
-
-
-@app.get("/check_data/", summary="Добавить адрес")
-def get_address(phone: str) -> Address | dict:
-    try:
-        address = redis_connection.get(phone)
+        address = await redis_conn.get(phone)
         if not address:
             return {"message": "По данному номеру не зарегистрировано адреса"}
         return {"address": address}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Ошибка" + str(e))
+    except redis.ConnectionError:
+        raise HTTPException(status_code=500, detail="Ошибка подключения к Redis")
--- models.py
+++ models.py
@@ -1,7 +1,20 @@
 from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from pydantic import Field
 
+class Settings(BaseSettings):
+    web_port: int = Field(..., env="WEB_PORT")
+    redis_host: str = Field(..., env="REDIS_HOST")
+    redis_port: int = Field(..., env="REDIS_PORT")
+    redis_db: int = Field(..., env="REDIS_DB")
+
+    class Config:
+        env_file = ".env"
 class Address(BaseModel):
     address: str
 
 class WriteAddress(Address):
     phone: str
+
+class MessageResponse(BaseModel):
+    message: str
